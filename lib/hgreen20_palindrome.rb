c2bfdf5f386736cc03bc56ca9be9ed90e4b58804
hannah green <hannah.green533@gmail.com>
2024-05-22 16:01:11 -0400

    Initialize repository

---
+++ lib/hgreen20_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hgreen20_palindrome/version"
+
+module Hgreen20Palindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hgreen20_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Hgreen20Palindrome
+  VERSION = "0.1.0"
+end
